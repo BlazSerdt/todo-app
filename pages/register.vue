f36de0271e85dece1b3cf7c08f5a541c9f831a04
blazserdt <blazserdtt@gmail.com>
2024-06-16 13:19:25 +0200

    Created Register page UI

--- pages/register.vue
+++ pages/register.vue
@@ -1,31 +1,36 @@
 <script setup>
   const username = ref('');
+  const email = ref('');
   const password = ref('');
 
-  function handleLogin(){
-    if (!username.value || !password.value) {
-      alert('Please fill out all fields.');
+  function handleRegister(){
+    if(!username.value || !email.value || !password.value){
+      alert("Please fill out all fields.");
       return;
     }
 
     console.log("User: " + username.value);
+    console.log("Email: " + email.value);
     console.log("Password: " + password.value);
 
     username.value = '';
+    email.value = '';
     password.value = '';
   }
 </script>
 
 <template>
   <div class="form-container">
-    <Header title="LOGIN"/>
+    <Header title="REGISTER"/>
     <form class="form">
       <label for="username" class="form-label">Username</label><br>
       <input type="text" id="username" name="username" class="form-input" v-model="username"><br>
+      <label for="email" class="form-label">E-Mail</label><br>
+      <input type="text" id="email" name="email" class="form-input" v-model="email"><br>
       <label for="password" class="form-label">Password</label><br>
       <input type="password" id="password" name="password" class="form-input" v-model="password"><br>
     </form>
-    <button class="submit-button" @click="handleLogin">Sign in</button>
-    <NuxtLink to="/register"><a class="link">Dont have an account yet? Register here</a></NuxtLink>
+    <button class="submit-button" @click="handleRegister">Register</button>
+    <NuxtLink to="/login"><a class="link">Already have an account? Login here</a></NuxtLink>
   </div>
 </template>
