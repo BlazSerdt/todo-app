149a23ab0117753465f8376fdfed605c211c7ae0
blazserdt <blazserdtt@gmail.com>
2024-06-16 12:17:32 +0200

    Created Login page UI

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  title: String
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  padding: 2px 5px 2px 5px;
+  margin: 0 0 12px 0;
+  color: white;
+  font-size: 28px;
+  font-weight: bold;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<script setup>
+  const username = ref('');
+  const password = ref('');
+
+  function handleLogin(){
+    if (!username.value || !password.value) {
+      alert('Please fill out all fields.');
+      return;
+    }
+
+    console.log("User: " + username.value);
+    console.log("Password: " + password.value);
+  }
+</script>
+
+<template>
+  <div class="login-container">
+    <Header title="LOGIN"/>
+    <form class="login-form">
+      <label for="username" class="form-label">Username</label><br>
+      <input type="text" id="username" name="username" class="form-input" v-model="username"><br>
+      <label for="password" class="form-label">Password</label><br>
+      <input type="password" id="password" name="password" class="form-input" v-model="password"><br>
+    </form>
+    <button class="login-button" @click="handleLogin">Sign in</button>
+    <a class="register-link">Dont have an account yet? Click here</a>
+  </div>
+</template>
